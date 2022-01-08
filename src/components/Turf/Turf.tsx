8ab0b2581c6923cf38af6c5b638fc321743795ca
agomesd <alex.gduguay@gmail.com>
2022-01-08 11:31:49 +0000

    Initial commit

---
+++ src/components/Turf/Turf.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export interface TurfProps {
+  data: {};
+}
+
+const Turf = (props: TurfProps) => {
+  return (
+    <div>
+      <h1>Turf</h1>
+    </div>
+  )
+}
+
+export default Turf;
